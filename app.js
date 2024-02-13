cc0804ad160dfbf4949b91681574866047d4aad1
mohamedkarrab2 <159923882+mohamedkarrab2@users.noreply.github.com>
2024-02-13 16:04:06 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+// EX1:
+//function max2(a,b) {
+  //  if(a>b)
+    //return a
+//else
+  //  return b;
+    
+//}
+//console.log(max2(5,8))
+//console.log(max2(15,8))
+//console.log(max2(18,18))
+//EX2:
+//function wr(char){
+//  return char
+//}
+
+//console.log(wr("bonjour"))
+
+//EX3:
+//n1=prompt("saisir un entier")
+//n2=prompt("saisir un 2émé entier")
+
+//Ex4:
+const container = document.getElementById("zone")
+
+function into(){
+    container.innerHTML = "Enter"
+}
+
+function out(){
+    container.innerHTML = "Out"
+}
+
+function bgColor(){
+    container.style.backgroundColor= "bgColor"
+}
+
+
+
+
+
+
+
